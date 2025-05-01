0a42038e584a071150df386fed8a304d4473815a
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-05-01 18:09:15 -0300

    feat: create variables

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "s3-bucket-ndeves-${terraform.workspace}"
+    bucket = "${var.bucket_name}-${terraform.workspace}"
 
     tags = {
         Name = "Bucket"
