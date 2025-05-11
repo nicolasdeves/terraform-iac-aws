c450d77fa7797260a81e29c4c2e506ac2c3bda21
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-05-11 20:15:38 -0300

    feat: add sqs public module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source      = "./modules/s3"
+  source      = "./modules/s3" #private (modules)
   bucket_name = "s3-bucket-ndeves"
   tags = {
     Name = "Bucket"
@@ -9,7 +9,7 @@ module "s3" {
 }
 
 module "cloudfront" {
-  source             = "./modules/cloudfront"
+  source             = "./modules/cloudfront" #private (modules)
   origin_id          = module.s3.bucket_id
   bucket_domain_name = module.s3.bucket_domain_name
   tags = {
@@ -19,4 +19,14 @@ module "cloudfront" {
   depends_on = [
     module.s3
   ]
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws" #public
+  name = "sqs-ndeves"
+  create_dlq = true
+
+  tags = {
+    Iac = true
+  }
 }
